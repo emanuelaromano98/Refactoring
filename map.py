e7ab9f8b44c6f34a0563747eca75f4a0ea52e3c2
emanuelaromano98 <emauelaromano1998@gmail.com>
2024-06-29 12:15:21 +0200

    added plot.html

--- map.py
+++ map.py
@@ -1,4 +1,11 @@
+# %%
 from function import *
+
+# %%
 df = load_data_with_column_order('df_migros.csv', ['city', 'population', 'coordinates', 'migros_count'])
+
+# %%
 df = format_df(df)
+
+# %%
 create_plot(df)
